1ff08cdb9d571c26246ccd5bcfa9ce8f01b15c6d
wznmickey <wznmickey@sjtu.edu.cn>
2020-09-25 23:27:53 +0800

    h1-1-1,h1-2-1,h1-3-1,h1-4-1,h1-5-1,h1-6-1,h1-7-1

---
+++ h1/ex3.m
@@ -0,0 +1,5 @@
+for i=1:1000 %Having know the total number 1000
+    t_s=input('');
+    t_d=t_s/(24*60*60);
+    disp(t_d);
+end
---
+++ h1/ex5.m
@@ -0,0 +1,8 @@
+%reference: wiki
+angel=7.2;
+meter=5000*160;
+meter_per_angel=meter/angel;
+c=meter_per_angel*360;
+disp(c/1000);
+r=c/2/pi;
+disp(r/1000);
