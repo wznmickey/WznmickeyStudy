548f7f67b00f955355964461b80c1696717ef8c9
wznmickey <wznmickey@sjtu.edu.cn>
2020-11-27 00:25:26 +0800

    update

--- h8/ex1.cpp
+++ h8/ex1.cpp
@@ -1,3 +1,21 @@
 #include<bits/stdc++.h>
 #include"assignment.h"
 using namespace std;
+void ex1_reverse_array()
+{
+    string input[100];
+    int i=0;
+    while (cin>>input[i]) i++;
+    i--;
+    while (i>=0)
+    {
+        cout<<input[i]<<" ";
+        i--;
+    }
+    return;
+}
+int main()
+{
+    ex1_reverse_array();
+    return 0;
+}
