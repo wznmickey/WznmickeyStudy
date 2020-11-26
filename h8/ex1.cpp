f5661ef11e3fd0659b7508b5fb749c18f587ea77
wznmickey <wznmickey@sjtu.edu.cn>
2020-11-27 01:21:44 +0800

    update

--- h8/ex1.cpp
+++ h8/ex1.cpp
@@ -1,3 +1,8 @@
+/*******************************
+    Written by WANGZINING
+    blog: https://wznmickey.com
+*******************************/
+
 #include "assignment.h"
 #include <bits/stdc++.h>
 using namespace std;
@@ -32,12 +37,49 @@ void ex1_reverse_vector( )
     }
     return;
 }
-void ex1_reverse_stack()
+void ex1_reverse_stack( )
 {
+    stack< string > input;
+    string          temp;
+    while ( cin >> temp ) { input.push( temp ); }
+    while ( ! input.empty( ) )
+    {
+        cout << input.top( ) << " ";
+        input.pop( );
+    }
     return;
 }
-int main( )
+void ex1_ordered_array( )
 {
-    ex1_reverse_vector( );
-    return 0;
+    string input [ 100 ];
+    int    i = 0;
+    while ( cin >> input [ i ] ) i++;
+    for ( int j = 0; j <= i; j++ ) { cout << input [ j ] << " "; }
+    return;
+}
+void ex1_ordered_vector( )
+{
+    vector< string > input;
+    string           temp;
+    int              i = 0;
+    while ( cin >> temp )
+    {
+        input.push_back( temp );
+        i++;
+    }
+    i--;
+    for ( int j = 0; j <= i; j++ ) { cout << input [ j ] << " "; }
+    return;
+}
+void ex1_ordered_queue( )
+{
+    queue< string > input;
+    string          temp;
+    while ( cin >> temp ) { input.push( temp ); }
+    while ( ! input.empty( ) )
+    {
+        cout << input.front( ) << " ";
+        input.pop( );
+    }
+    return;
 }
