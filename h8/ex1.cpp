785dadba6ccd3d3a4225454cb045b020dc20fbff
wznmickey <wznmickey@sjtu.edu.cn>
2020-11-27 00:25:36 +0800

    update

--- h8/ex1.cpp
+++ h8/ex1.cpp
@@ -1,21 +1,39 @@
-#include<bits/stdc++.h>
-#include"assignment.h"
+#include "assignment.h"
+#include <bits/stdc++.h>
 using namespace std;
-void ex1_reverse_array()
+void ex1_reverse_array( )
 {
-    string input[100];
-    int i=0;
-    while (cin>>input[i]) i++;
+    string input [ 100 ];
+    int    i = 0;
+    while ( cin >> input [ i ] ) i++;
     i--;
-    while (i>=0)
+    while ( i >= 0 )
     {
-        cout<<input[i]<<" ";
+        cout << input [ i ] << " ";
         i--;
     }
     return;
 }
-int main()
+void ex1_reverse_vector( )
 {
-    ex1_reverse_array();
+    vector< string > input;
+    string           temp;
+    int              i = 0;
+    while ( cin >> temp )
+    {
+        input [ i ] = temp;
+        i++;
+    }
+    i--;
+    while ( i >= 0 )
+    {
+        cout << input [ i ] << " ";
+        i--;
+    }
+    return;
+}
+int main( )
+{
+    ex1_reverse_vector( );
     return 0;
 }
