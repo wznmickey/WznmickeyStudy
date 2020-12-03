95580ac45a0c289255d75a87e73504fdd6cdbeae
wznmickey <first@wznmickey.com>
2020-12-03 21:08:38 +0800

    backup

--- h8/ex2.cpp
+++ h8/ex2.cpp
@@ -1,7 +1,255 @@
-/**********************************
+/***********************************
     Written by WANGZINING
     blog: https://wznmickey.com
-**********************************/
+***********************************/
 #include "assignment.h"
 #include "paint.h"
+#include <GL/glut.h>
 #include <bits/stdc++.h>
+using namespace std;
+
+// Point class
+Point::Point( )
+{
+    x = 0;
+    y = 0;
+    return;
+}
+Point::Point( double x, double y )
+{
+    this->x = x;
+    this->y = y;
+    return;
+}
+void Point::move( double dx, double dy )
+{
+    x += dx;
+    y += dy;
+    return;
+}
+void Point::zoom( double r )
+{
+    x *= r;
+    y *= r;
+    return;
+}
+
+// Triangle class
+Triangle::Triangle( )
+{
+    p1 = Point( 0, 0 );
+    p2 = Point( 0, 0 );
+    p3 = Point( 0, 0 );
+    return;
+}
+Triangle::Triangle( Point p1, Point p2, Point p3 )
+{
+    this->p1 = p1;
+    this->p2 = p2;
+    this->p3 = p3;
+    return;
+}
+void Triangle::move( double dx, double dy )
+{
+    p1.move( dx, dy );
+    p2.move( dx, dy );
+    p3.move( dx, dy );
+    return;
+}
+void Triangle::zoom( double r )
+{
+    p1.zoom( r );
+    p2.zoom( r );
+    p3.zoom( r );
+    return;
+}
+double Triangle::area( )
+{
+    // using Heron's formula
+    double a = p1.length( p2 );
+    double b = p2.length( p3 );
+    double c = p3.length( p1 );
+    double p = a + b + c;
+    return sqrt( p * ( p - a ) * ( p - b ) * ( p - c ) );
+}
+void Triangle::draw( )
+{
+    glColor3f( 255, 255, 255 );
+    glBegin( GL_QUADS );
+    glVertex2f( p1.getx( ), p1.gety( ) );
+    glVertex2f( p2.getx( ), p2.gety( ) );
+    glVertex2f( p3.getx( ), p3.gety( ) );
+    glEnd( );
+    return;
+}
+
+// Rectangle class
+Rectangle::Rectangle( )
+{
+    p1 = Point( 0, 0 );
+    p2 = Point( 0, 0 );
+    return;
+}
+Rectangle::Rectangle( Point p1, Point p2 )
+{
+    this->p1 = p1;
+    this->p2 = p2;
+    return;
+}
+void Rectangle::move( double dx, double dy )
+{
+    p1.move( dx, dy );
+    p2.move( dx, dy );
+    return;
+}
+void Rectangle::zoom( double r )
+{
+    p1.zoom( r );
+    p2.zoom( r );
+    return;
+}
+double Rectangle::area( )
+{
+    return fabs( ( p1.getx( ) - p2.getx( ) ) ) * fabs( ( p1.gety( ) - p2.gety( ) ) );
+}
+void Rectangle::draw( )
+{
+    glColor3f( 255, 255, 255 );
+    glBegin( GL_QUADS );
+    glVertex2f( p1.getx( ), p1.gety( ) );
+    glVertex2f( p2.getx( ), p1.gety( ) );
+    glVertex2f( p2.getx( ), p2.gety( ) );
+    glVertex2f( p1.getx( ), p2.gety( ) );
+    glEnd( );
+    return;
+}
+
+// Parallelogram class
+Parallelogram::Parallelogram( )
+{
+    p1 = Point( 0, 0 );
+    p2 = Point( 0, 0 );
+    p3 = Point( 0, 0 );
+    p4 = Point( 0, 0 );
+    return;
+}
+Parallelogram::Parallelogram( Point p1, Point p2, Point p3, Point p4 )
+{
+    this->p1 = p1;
+    this->p2 = p2;
+    this->p3 = p3;
+    this->p4 = p4;
+    return;
+}
+void Parallelogram::move( double dx, double dy )
+{
+    p1.move( dx, dy );
+    p2.move( dx, dy );
+    p3.move( dx, dy );
+    p4.move( dx, dy );
+    return;
+}
+void Parallelogram::zoom( double r )
+{
+    p1.zoom( r );
+    p2.zoom( r );
+    p3.zoom( r );
+    p4.zoom( r );
+    return;
+}
+double Parallelogram::area( )
+{
+    Triangle a, b;
+    a = Triangle( p1, p2, p3 );
+    b = Triangle( p2, p3, p4 );
+    return a.area( ) + b.area( );
+}
+void Parallelogram::draw( )
+{
+    glColor3f( 255, 255, 255 );
+    glBegin( GL_QUADS );
+    glVertex2f( p1.getx( ), p1.gety( ) );
+    glVertex2f( p2.getx( ), p2.gety( ) );
+    glVertex2f( p3.getx( ), p3.gety( ) );
+    glVertex2f( p4.getx( ), p4.gety( ) );
+    glEnd( );
+    return;
+}
+
+// Trapezium class
+Trapezium::Trapezium( )
+{
+    p1 = Point( 0, 0 );
+    p2 = Point( 0, 0 );
+    p3 = Point( 0, 0 );
+    p4 = Point( 0, 0 );
+    return;
+}
+Trapezium::Trapezium( Point p1, Point p2, Point p3, Point p4 )
+{
+    this->p1 = p1;
+    this->p2 = p2;
+    this->p3 = p3;
+    this->p4 = p4;
+    return;
+}
+void Trapezium::move( double dx, double dy )
+{
+    p1.move( dx, dy );
+    p2.move( dx, dy );
+    p3.move( dx, dy );
+    p4.move( dx, dy );
+    return;
+}
+void Trapezium::zoom( double r )
+{
+    p1.zoom( r );
+    p2.zoom( r );
+    p3.zoom( r );
+    p4.zoom( r );
+    return;
+}
+double Trapezium::area( )
+{
+    Triangle a, b;
+    a = Triangle( p1, p2, p3 );
+    b = Triangle( p2, p3, p4 );
+    return a.area( ) + b.area( );
+}
+void Trapezium::draw( )
+{
+    glColor3f( 255, 255, 255 );
+    glBegin( GL_QUADS );
+    glVertex2f( p1.getx( ), p1.gety( ) );
+    glVertex2f( p2.getx( ), p2.gety( ) );
+    glVertex2f( p3.getx( ), p3.gety( ) );
+    glVertex2f( p4.getx( ), p4.gety( ) );
+    glEnd( );
+    return;
+}
+void TimeStep( int n )
+{
+    glutTimerFunc( n, TimeStep, n );
+    glutPostRedisplay( );
+}
+void glDraw( )
+{
+    double static width = 1, height = 1.5, owidth = .175;
+    Trapezium zh( { 0, 25 }, { 0, 5 }, { 15, 25 }, { 15, 26 } );
+    zh.zoom( 2 );
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+    zh.draw( );
+    glutSwapBuffers( );
+    glFlush( );
+}
+int main( int argc, char *argv [] )
+{
+    glutInit( &argc, argv );
+    glutInitDisplayMode( GLUT_RGB | GLUT_SINGLE );
+    glutCreateWindow( "Demo draw" );
+    glClearColor( 1.0, 1.0, 1.0, 0.0 );
+    glClear( GL_COLOR_BUFFER_BIT );
+    glutDisplayFunc( glDraw );
+    glutTimerFunc( 25, TimeStep, 25 );
+    glutMainLoop( );
+}
--- h8/paint.h
+++ h8/paint.h
@@ -1,34 +1,61 @@
-/*******************************
+/***********************************
     Written by WANGZINING
     blog: https://wznmickey.com
-*******************************/
+***********************************/
 #ifndef PAINT
 #define PAINT
-
+#include<math.h>
 class Point
 {
 public:
+    Point( );
+    ~Point( )
+    {
+        return;
+    };
+    Point( double x, double y );
+    void   move( double dx, double dy );
+    void   zoom( double r );
+    double getx( )
+    {
+        return x;
+    }
+    double gety( )
+    {
+        return y;
+    }
+    double length( Point a )
+    {
+        return sqrt( ( a.x - x ) * ( a.x - x ) + ( a.y - y ) * ( a.y - y ) );
+    }
+
+private:
     double x, y;
 };
 
 class Shape
 {
 public:
-    virtual ~Shape() = 0;
-    virtual void move(double dx, double dy);
-    virtual void zoom(double x);
-    virtual float area();
+    virtual ~Shape( ) = 0;
+    virtual void   move( double dx, double dy );
+    virtual void   zoom( double x );
+    virtual double area( );
+    virtual void   draw( );
 };
 
 class Triangle : public Shape
 {
 public:
-    Triangle();
-    Triangle(Point p1, Point p2, Point p3);
-    ~Triangle();
-    void move(double dx, double dy);
-    void zoom(double x);
-    float area();
+    Triangle( );
+    Triangle( Point p1, Point p2, Point p3 );
+    ~Triangle( )
+    {
+        return;
+    }
+    void   move( double dx, double dy );
+    void   zoom( double x );
+    double area( );
+    void   draw( );
 
 private:
     Point p1, p2, p3;
@@ -37,12 +64,16 @@ private:
 class Rectangle : public Shape
 {
 public:
-    Rectangle();
-    Rectangle(Point p1, Point p2);
-    ~Rectangle();
-    void move(double dx, double dy);
-    void zoom(double x);
-    float area();
+    Rectangle( );
+    Rectangle( Point p1, Point p2 );
+    ~Rectangle( )
+    {
+        return;
+    }
+    void   move( double dx, double dy );
+    void   zoom( double x );
+    double area( );
+    void   draw( );
 
 private:
     Point p1, p2;
@@ -51,18 +82,36 @@ private:
 class Parallelogram : public Shape
 {
 public:
-    Parallelogram();
-    Parallelogram(Point p1, Point p2, Point p3);
-    ~Parallelogram();
-    void move(double dx, double dy);
-    void zoom(double x);
-    float area();
+    Parallelogram( );
+    Parallelogram( Point p1, Point p2, Point p3, Point p4 );
+    ~Parallelogram( )
+    {
+        return;
+    }
+    void   move( double dx, double dy );
+    void   zoom( double x );
+    double area( );
+    void   draw( );
 
 private:
-    Point p1, p2, p3;
-}
+    Point p1, p2, p3, p4;
+};
 
 class Trapezium : public Shape
 {
-}
-#endif //PAINT
+public:
+    Trapezium( );
+    Trapezium( Point p1, Point p2, Point p3, Point p4 );
+    ~Trapezium( )
+    {
+        return;
+    }
+    void   move( double dx, double dy );
+    void   zoom( double x );
+    double area( );
+    void   draw( );
+
+private:
+    Point p1, p2, p3, p4;
+};
+#endif // PAINT
