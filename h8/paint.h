d8015d6dcbb039055a83974c0a2deb7159ae7d11
wznmickey <first@wznmickey.com>
2020-12-03 19:01:04 +0800

    h8ex2backup

--- h8/paint.h
+++ h8/paint.h
@@ -8,20 +8,61 @@
 class Point
 {
 public:
-    double x, y
+    double x, y;
 };
-class Triangle
+
+class Shape
 {
 public:
-    Triangle( );
-    Triangle( Point p1, Point p2, Point p3 );
-    ~Triangle( );
-    void  move( double dx, double dy );
-    void  zoom( double x );
-    float area( );
+    virtual ~Shape() = 0;
+    virtual void move(double dx, double dy);
+    virtual void zoom(double x);
+    virtual float area();
+};
+
+class Triangle : public Shape
+{
+public:
+    Triangle();
+    Triangle(Point p1, Point p2, Point p3);
+    ~Triangle();
+    void move(double dx, double dy);
+    void zoom(double x);
+    float area();
 
 private:
-    Point p1, Point p2, Point p3;
+    Point p1, p2, p3;
 };
-class Rectangle
-#endif
+
+class Rectangle : public Shape
+{
+public:
+    Rectangle();
+    Rectangle(Point p1, Point p2);
+    ~Rectangle();
+    void move(double dx, double dy);
+    void zoom(double x);
+    float area();
+
+private:
+    Point p1, p2;
+};
+
+class Parallelogram : public Shape
+{
+public:
+    Parallelogram();
+    Parallelogram(Point p1, Point p2, Point p3);
+    ~Parallelogram();
+    void move(double dx, double dy);
+    void zoom(double x);
+    float area();
+
+private:
+    Point p1, p2, p3;
+}
+
+class Trapezium : public Shape
+{
+}
+#endif //PAINT
